8e414f2b6a443c39d126e184c48f88f2012469fd
Hattan Shobokshi <hattan@gmail.com>
2020-05-22 13:31:54 -0700

    added check for pr

--- index.js
+++ index.js
@@ -3,25 +3,67 @@ const core = require('@actions/core')
 const { Toolkit } = require('actions-toolkit')
 
 Toolkit.run(async tools => {
-  // Get the file
-  tools.log.debug('Initialize test')
-
-  // Create the new issue
+  
   try {
-    let comment = await tools.github.issues.createComment({
-        issue_number: '30',
+    if(!context.payload.pull_request){
+        tools.log.warn('Not a pull request skipping verification!');
+        return;
+    }
+    tools.log.debug('Starting Pull Request Verification!');
+
+    let isLinked=false;
+    let invalidIssue=false;
+    let body = context.payload.pull_request.body;
+    const re = /#(.*?)[\s]/g;
+    const matches = body.match(re);
+
+    if(matches){
+        matches.forEach(match => {
+            var issueId = match.replace('#','').trim();
+            let issue = github.issues.get({
+                owner: context.repo.owner,
+                repo: context.repo.repo,
+                issue_number: issueId,
+            });
+            if(issue){
+                isLinked = true;
+                invalidIssue=true;
+            }
+        });
+    }
+
+    if(!isLinked){
+        let pull = await tools.github.issues.listEvents({
         owner: tools.context.repo.owner,
         repo: tools.context.repo.repo,
-        body: 'test'
-    }) 
-    
+        issue_number: tools.context.payload.pull_request.number 
+        });
+        if(pull.data){
+        pull.data.forEach(item => {
+            if (item.event == "connected"){
+            isLinked = true;
+            }
+        });
+        }
+    }
 
-
-   
-    tools.log.success(`Created comment ${comment}`)
+    if(isLinked){
+        tools.log.success("Success! Linked Issue Found!");
+    }
+    else{
+        await github.issues.createComment({
+            issue_number: context.payload.pull_request.number,
+            owner: context.repo.owner,
+            repo: context.repo.repo,
+            body: 'Build Error! No Linked Issue found. Please link an issue or mention it in the body using #<issue_id>'
+        });
+        tools.log.error("No Linked Issue Found!");
+        core.setFailed("No Linked Issue Found!");
+        tools.exit.failure() 
+    }
   } catch (err) {
     // Log the error message
-    const errorMessage = `An error occurred while creating the comment.`
+    const errorMessage = `An error occurred while creating the issue. This might be caused by a malformed issue title, or a typo in the labels or assignees. Check ${template}!`
     tools.log.error(errorMessage)
     tools.log.error(err)
 
@@ -32,4 +74,6 @@ Toolkit.run(async tools => {
     core.setFailed(errorMessage + '\n\n' + err.message)
     tools.exit.failure()
   }
+}, {
+  secrets: ['GITHUB_TOKEN']
 })
