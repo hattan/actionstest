03915eb3d28029c828500a0fa8d7879b5584c646
Hattan Shobokshi <hattan@gmail.com>
2020-05-24 23:44:03 -0700

    testing webv from entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,9 @@ filename=$2
 
 echo "server = $server filename=$filename"
 
-dotnet --list-runtimes
+dotnet /app/webvalidate --host https://www.microsoft.com --files msft.json
+
+##dotnet --list-runtimes
 
 #dotnet webvalidate.dll --host https://www.microsoft.com --files msft.json
 #dotnet webvalidate.dll --host $server --files $filename
