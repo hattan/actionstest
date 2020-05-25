e35da94757afc82c69331ecb25ef284a9140cb37
Hattan Shobokshi <hattan@gmail.com>
2020-05-25 00:27:36 -0700

    changing to webv beta base image

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM retaildevcrew/webvalidate
+FROM retaildevcrew/webvalidate:beta
 
 WORKDIR /app
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,6 @@ echo "server = $server filename=$filename"
 
 cd /app
 
-dotnet /app/webvalidate.dll --host $server --files $filename
+dotnet /app/webvalidate.dll --server $server --files $filename
 
 cd /github/workspace
