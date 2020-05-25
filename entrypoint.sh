e16c18099bc906ac828d3ef534bdf4a829d86485
Hattan Shobokshi <hattan@gmail.com>
2020-05-25 00:18:11 -0700

    moving to pushd

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,12 +7,8 @@ filename=$2
 echo "server = $server filename=$filename"
 
 #sleep 300
-cd /app
+pushd /app
+
 dotnet /app/webvalidate.dll --host https://www.microsoft.com --files msft.json
 
-#ls
-
-##dotnet --list-runtimes
-
-#dotnet webvalidate.dll --host https://www.microsoft.com --files msft.json
-#dotnet webvalidate.dll --host $server --files $filename
+popd
