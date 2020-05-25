ed2d29884c97b47f1e13fe219e926851502ce5b4
Hattan Shobokshi <hattan@gmail.com>
2020-05-25 00:10:46 -0700

    reverting back to dotnet validate

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,9 +7,9 @@ filename=$2
 echo "server = $server filename=$filename"
 
 #sleep 300
-#dotnet webvalidate.dll --host https://www.microsoft.com --files msft.json
+dotnet webvalidate.dll --host https://www.microsoft.com --files msft.json
 
-ls
+#ls
 
 ##dotnet --list-runtimes
 
