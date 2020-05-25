93611f51a2ab8ea4291a6ccb88f35c84b7a64989
Hattan Shobokshi <hattan@gmail.com>
2020-05-25 00:13:49 -0700

    updating webvalidate path

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ filename=$2
 echo "server = $server filename=$filename"
 
 #sleep 300
-dotnet webvalidate.dll --host https://www.microsoft.com --files msft.json
+dotnet /app/webvalidate.dll --host https://www.microsoft.com --files msft.json
 
 #ls
 
